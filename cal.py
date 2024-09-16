44559823c803944f2e48cda4968e8ff3dbd98762
Parker Gustavson <gustapar000@alpinesd.org>
2024-09-16 12:04:22 -0600

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,14 @@
+while True:
+    a = input("Operation: ")
+    b = float(input("What is your first number: "))
+    c = float(input("What is your second number: "))
+    if a == "+" :
+        print (b + c)
+    elif a == "-" :
+        print (b - c)
+    elif a == "*" :
+        print (b * c)
+    elif a == "/" :
+        print (b / c)
+    elif a == "**" :
+        print (b ** c)
